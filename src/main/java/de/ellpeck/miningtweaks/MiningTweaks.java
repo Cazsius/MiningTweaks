7d1bf2af6e04d126af4faa36ea656b66e622dbd1
Zeitheron <zeitheron.dr@gmail.com>
2021-01-21 20:29:17 +0200

    Port for 1.16.2-1.16.5; Implemented a patch to undo all actions when exiting a world.

--- src/main/java/de/ellpeck/miningtweaks/MiningTweaks.java
+++ src/main/java/de/ellpeck/miningtweaks/MiningTweaks.java
@@ -1,7 +1,9 @@
 package de.ellpeck.miningtweaks;
 
 import com.google.common.collect.Lists;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ToolType;
@@ -9,10 +11,14 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
@@ -22,17 +28,40 @@ public class MiningTweaks {
 
   public static final String MOD_ID = "miningtweaks";
 
+  static final LinkedList<Runnable> UNDO = new LinkedList<>();
+
   public MiningTweaks() {
     ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_SPEC);
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onConfigurationChangedEvent);
     EVENT_BUS.addListener(this::serverstart);
+    EVENT_BUS.addListener(this::serverstop);
   }
 
   public static void handleConfig() {
+    Field stateHardness = AbstractBlock.AbstractBlockState.class.getDeclaredFields()[5];
+    stateHardness.setAccessible(true);
+
     ConfigHandler.tweak_hardness.get().forEach(s -> {
       String[] split = s.split("@");
       Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(split[0]));
-      block.blockHardness = Float.parseFloat(split[1]);
+      float blockHardness = Float.parseFloat(split[1]);
+      block.getStateContainer().getValidStates().forEach(state -> {
+        try {
+          final float prevHardness = stateHardness.getFloat(state);
+          final BlockState fstate = state;
+
+          stateHardness.setFloat(state, blockHardness);
+          UNDO.push(()->{
+            try {
+              stateHardness.setFloat(fstate, prevHardness);
+            } catch(ReflectiveOperationException e) {
+              throw new RuntimeException("oh no", e);
+            }
+          });
+        } catch(ReflectiveOperationException e) {
+          throw new RuntimeException("oh no", e);
+        }
+      });
     });
 
     ConfigHandler.tweak_mining_level.get().forEach(s -> {
@@ -48,8 +77,15 @@ public class MiningTweaks {
     handleConfig();
   }
 
+  public void serverstop(FMLServerStoppedEvent e) {
+    UNDO.forEach(Runnable::run);
+    UNDO.clear();
+  }
+
   public void onConfigurationChangedEvent(ModConfig.ModConfigEvent event) {
     if (MOD_ID.equals(event.getConfig().getModId())) {
+      UNDO.forEach(Runnable::run);
+      UNDO.clear();
       handleConfig();
     }
   }
@@ -78,7 +114,7 @@ public class MiningTweaks {
 
       tweak_mining_level =
               builder
-                      .comment("The blocks whose mining level should be modified. This needs to be the registry name of the block followed by an @ followed by the tool class ('pickaxe', 'axe' or 'shovel') followed by an @ followed by the harvest level (0 = wood, 1 = stone, 2 = iron, 3 = diamond, custom levels work), so for example: 'minecraft:stone@pickaxe@2'")
+                      .comment("The blocks whose mining level should be modified. This needs to be the registry name of the block followed by an @ followed by the tool class ('pickaxe', 'axe' or 'shovel') followed by an @ followed by the harvest level (0 = wood, 1 = stone, 2 = iron, 3 = diamond, 4 = netherite, custom levels work), so for example: 'minecraft:stone@pickaxe@2'")
                       .defineList("tweak_mining_level", Lists.newArrayList(), String.class::isInstance);
       builder.pop();
     }
--- src/main/java/de/ellpeck/miningtweaks/Util.java
+++ src/main/java/de/ellpeck/miningtweaks/Util.java
@@ -22,17 +22,33 @@ public class Util {
 		}
 	}
 
-	public static void setHarvestLevel(Block b, int level) {
+	public static void setHarvestLevel(final Block b, int level) {
 		try {
+			final int prevLevel = harvestLevel.getInt(b);
 			harvestLevel.setInt(b, level);
+			MiningTweaks.UNDO.push(()->{
+				try {
+					harvestLevel.setInt(b, prevLevel);
+				} catch(ReflectiveOperationException e) {
+					throw new RuntimeException("oh no", e);
+				}
+			});
 		} catch (Exception e) {
 			throw new RuntimeException("oh no", e);
 		}
 	}
 
-	public static void setHarvestTool(Block b, ToolType tool) {
+	public static void setHarvestTool(final Block b, ToolType tool) {
 		try {
+			final Object prevTool = harvestTool.get(b);
 			harvestTool.set(b, tool);
+			MiningTweaks.UNDO.push(()->{
+				try {
+					harvestTool.set(b, prevTool);
+				} catch(ReflectiveOperationException e) {
+					throw new RuntimeException("oh no", e);
+				}
+			});
 		} catch (Exception e) {
 			throw new RuntimeException("oh no", e);
 		}
